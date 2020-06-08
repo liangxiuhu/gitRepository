2377c2103214a39ad9c358c33cde970c55ba9806
liangxiuhu <liangxiuhu594@163.com>
2020-06-08 08:51:45 +0800

    新增工具类--爬取网站内容

--- transmigration/src/main/java/com/lxh/util/ConnectionJsoupUtil.java
+++ transmigration/src/main/java/com/lxh/util/ConnectionJsoupUtil.java
@@ -1,4 +1,21 @@
 package com.lxh.util;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class ConnectionJsoupUtil {
+
+    /**
+     *
+     * @param address
+     * @return
+     */
+    public static String connection(String address){
+        //Jsoup：用来对HTML进行解析
+
+        Document doc = Jsoup.parse(address);
+
+        return "";
+    }
 }
